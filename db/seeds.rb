10ebdddfb904238fd2de07e78e03de631eed7e58
Sylvain Bohrer <a3e7d6@gmail.com>
2024-03-06 16:13:53 +0100

    ajout de 5 enregistrements valides dans les deux tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create(name: "Le Beurre Noisette", address: "Paris 15ème", category: "french")
+Restaurant.create(name: "Aux Trois Petits Cochons", address: "Paris 15ème", category: "french")
+Restaurant.create(name: "Les Cocottes de Christian Constant", address: "Paris 15ème", category: "french")
+Restaurant.create(name: "Le P'tit Caillou", address: "Paris 15ème", category: "french")
+Restaurant.create(name: "Le Casse-Noix", address: "Paris 15ème", category: "french")
+
+Review.create(rating: 4, content: "Délicieux repas, je recommande vivement !", restaurant_id: 1)
+Review.create(rating: 5, content: "Service exceptionnel et plats incroyables.", restaurant_id: 2)
+Review.create(rating: 4, content: "Une expérience gastronomique inoubliable.", restaurant_id: 3)
+Review.create(rating: 3, content: "Des plats délicieux mais un service un peu lent.", restaurant_id: 4)
+Review.create(rating: 5, content: "Le meilleur restaurant que j'ai jamais essayé !", restaurant_id: 5)
