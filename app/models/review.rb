9cca77a1a2ce1f2e2d199d8b9d9c8301ab592b91
Sylvain Bohrer <a3e7d6@gmail.com>
2024-03-06 17:03:42 +0100

    rating cant be blank

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :rating, presence: true
+  # validates :rating, presence: { message: "can't be blank" }
+  # puts review.errors.full_messages
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
---
+++ db/migrate/20240306154902_change_column_rating_to_not_null.rb
@@ -0,0 +1,5 @@
+class ChangeColumnRatingToNotNull < ActiveRecord::Migration[7.1]
+  def change
+    change_column_null :reviews, :rating, false
+  end
+end
